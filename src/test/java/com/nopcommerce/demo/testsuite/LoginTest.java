4c27a1125a50dde63be43b1a1b9a35cc23497e84
avinashp22 <avinashp22@gmail.com>
2023-12-02 16:07:24 +0000

    sample-maven-project

---
+++ src/test/java/com/nopcommerce/demo/testsuite/LoginTest.java
@@ -0,0 +1,11 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+@Test
+public void verifyUserShouldNavigatetoLoginPageSuccessfully(){}
+
+}
